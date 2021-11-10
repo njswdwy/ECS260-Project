ee90753d75cbd61b568ee062f63bbddd236eb7df
xiao kaixin <xiaokaixin@xiaos-MacBook-Pro.local>
2021-11-10 11:22:31 -0800

    feat: add data cleaning scriot

---
+++ cleaner/index.js
@@ -0,0 +1,34 @@
+var fs = require('fs').promises;
+var parse = require('csv-parse/lib/sync');
+var stringify = require('csv-stringify');
+    
+
+(async function () {
+    const fileContent = await fs.readFile(__dirname+'../PRs_dataset/project_list.csv');
+    const records = parse(fileContent, {columns: true});
+    const cleanedData = records.reduce((acc, item) => {
+        if (!item['PR Number'] || item['PR Number'] === '0' || item['PR Number'] === 0) {
+            return acc;
+        }
+        const endDate = item['Project Enddate'].split('/');
+        const endYear = parseInt(endDate[0]);
+        const endMonth = parseInt(endDate[1]);
+        const endDay = parseInt(endDate[2]);
+        const prDate = item['First PR Created Time'].split(' ')[0].split('-');
+        const prYear = parseInt(prDate[0]);
+        const prMonth = parseInt(prDate[1]);
+        const prDay = parseInt(prDate[2]);
+        console.log(endYear, endMonth, endDay, prYear, prMonth, prDay);
+        if (prYear < endYear || (prYear === endYear && prMonth < endMonth) ||
+        (prYear === endYear && prMonth === endMonth && prDay < endDay)) {
+            return [...acc, item];
+        }
+        return acc;
+    }, []);
+
+    stringify(cleanedData, {
+        header: true
+    }, function (err, output) {
+        fs.writeFile(__dirname+'../PRs_dataset/cleaned_project_list.csv', output);
+    })
+})();
