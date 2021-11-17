597f055ca96d5e4896707b87bb1f865bdd0908c4
jyu25utk <jyu25@vols.utk.edu>
2021-11-17 10:38:21 -0800

    Retrieved all PR datalimit in incubation phase

--- PR_Jinxiao.py
+++ PR_Jinxiao.py
@@ -7,10 +7,10 @@ import requests
 import time
 from urllib3 import Retry
 
-access_token = "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"
+access_token = ""
 
 
-f = open('RP_Log.txt', 'a')
+f = open('RP_Log_Jinxiao.txt', 'a')
 f.write((str(datetime.datetime.now())))
 
 
@@ -20,16 +20,20 @@ cleanedProjectList = pd.read_csv('PRs_dataset/cleaned_project_list.csv')
 #get pr data
 def extract_project_PRs(pr_list, sDate, eDate):
     df_PRs = pd.DataFrame()
+    dateLimit = False
 
     merged_pr_count = 0
     for pr in pr_list:
             try:
                 # print(g.rate_limiting)
                 print(f'Extracting data from PR # {pr.number}')
-                f.write('Extracting data from PR # {number}\n'.format(number = pr.number))
                 PRsDate = pr.created_at
                 if sDate <= PRsDate and eDate >= PRsDate:
                     print('Yes')
+                    dateLimit = True
+                elif dateLimit:
+                    print('Not in date')
+                    break
                 else:
                     print('No')
                     continue
@@ -56,7 +60,7 @@ def extract_project_PRs(pr_list, sDate, eDate):
                 f.write(str(e.status))
                 f.write('Rate limit exceeded')
                 f.write('\n')
-                time.sleep(5)
+                time.sleep(3600)
                 continue
             except BadCredentialsException as e:
                 f.write(str(e.status))
@@ -92,13 +96,52 @@ def extract_project_PRs(pr_list, sDate, eDate):
     return df_PRs
 
 
-for i in range(5, len(cleanedProjectList)):
-    projectName = cleanedProjectList['Project Full Name'][i]
-    startDate = datetime.datetime.strptime(cleanedProjectList['Project Startdate'][i], '%m/%d/%Y')
-    endDate = datetime.datetime.strptime(cleanedProjectList['Project Enddate'][i], '%m/%d/%Y')
-    print(projectName + ' ' + str(startDate) + ' ' + str(endDate))
-    g = Github(access_token, retry = Retry (total = 15, status_forcelist = (500, 502, 504), backoff_factor = 0.3), timeout=6000, per_page=100)
-    repo = g.get_repo(projectName)
-    pr_list = repo.get_pulls(state = 'closed', sort = 'created')
-    projectDF = extract_project_PRs(pr_list, startDate, endDate)
-    projectDF.to_csv('PRs_dataset/' + projectName + '.csv', sep = ',', encoding = 'utf-8', index = True)
+for i in range(0, 11):
+    try:
+        projectName = cleanedProjectList['Project Full Name'][i]
+        startDate = datetime.datetime.strptime(cleanedProjectList['Project Startdate'][i], '%m/%d/%Y')
+        endDate = datetime.datetime.strptime(cleanedProjectList['Project Enddate'][i], '%m/%d/%Y')
+        print(projectName + ' ' + str(startDate) + ' ' + str(endDate))
+        f.write(projectName + '\n')
+        g = Github(access_token, retry = Retry (total = 15, status_forcelist = (500, 502, 504), backoff_factor = 0.3), timeout=6000, per_page=100)
+        repo = g.get_repo(projectName)
+        pr_list = repo.get_pulls(state = 'closed', sort = 'created')
+        projectDF = extract_project_PRs(pr_list, startDate, endDate)
+        projectDF.to_csv('PRs_dataset/' + projectName + '.csv', sep = ',', encoding = 'utf-8', index = True)
+    except RateLimitExceededException as e:
+        print('Rate limit exceeded')
+        f.write(str(e.status))
+        f.write('Rate limit exceeded')
+        f.write('\n')
+        time.sleep(3600)
+        continue
+    except BadCredentialsException as e:
+        f.write(str(e.status))
+        f.write('Bad credentials exception')
+        f.write('\n')
+        break
+    except UnknownObjectException as e:
+        f.write(str(e.status))
+        f.write('Unknown object exception')
+        f.write('\n')
+        break
+    except GithubException as e:
+        f.write(str(e.status))
+        f.write('General exception')
+        f.write('\n')
+        break
+    except requests.exceptions.ConnectionError as e:
+        f.write('Retries limit exceeded')
+        f.write(str(e))
+        f.write('\n')
+        time.sleep(10)
+        continue
+    except requests.exceptions.Timeout as e:
+        f.write(str(e))
+        f.write('Time out exception')
+        f.write('\n')
+        time.sleep(10)
+        continue
+    except:
+        f.write('exception')
+        continue
