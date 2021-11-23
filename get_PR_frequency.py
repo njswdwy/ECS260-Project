5beda58ddd01bbea09f857fbde02be4cf2dfd55c
jyu25utk <jyu25@vols.utk.edu>
2021-11-23 12:09:29 -0800

    add PR frequencies to project list

---
+++ get_PR_frequency.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import datetime
+from dateutil import relativedelta
+
+cleanedProjectList = pd.read_csv('cleaned_project_w_pr.csv', sep = ',')
+
+def round_school(x):
+    i, f = divmod(x, 1)
+    return int(i + ((f >= 0.5) if (x > 0) else (f > 0.5)))
+
+for i in range(0, len(cleanedProjectList)):
+    startDate = datetime.datetime.strptime(cleanedProjectList['Project Startdate'][i], '%m/%d/%y')
+    endDate = datetime.datetime.strptime(cleanedProjectList['Project Enddate'][i], '%m/%d/%y')
+    r = relativedelta.relativedelta(endDate, startDate)
+    months = (r.years * 12) + r.months + 1
+    PRFrequency = round_school(cleanedProjectList['PR Number'][i] / months) 
+    cleanedProjectList['Merge Frequency'][i] = PRFrequency
+
+cleanedProjectList.to_csv('cleaned_project_w_frequncy.csv', sep = ',', encoding = 'utf-8', index = True)
